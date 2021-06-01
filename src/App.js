ce2f751f4c9372699047e4e9dfeeb39e7628478e
Ariangel Díaz Espaillat <adex0494@gmail.com>
2021-06-01 08:45:26 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
+import BasicForm from "../src/components/UI/BasicForm";
+
+function App() {
+  const [showCart, setShowCart] = useState(false);
+
+  const doShowCart = () => {
+    setShowCart(true);
+  };
+
+  const hideCart = () => {
+    setShowCart(false);
+  };
+
+  return (
+    <CartProvider>
+      {showCart && <Cart onHideCart={hideCart} />}
+      <Header onShowCart={doShowCart}></Header>
+      <main>
+        <Meals />
+      </main>
+    </CartProvider>
+  );
+}
+
+export default App;
